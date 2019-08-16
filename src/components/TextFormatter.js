9405686a7829f95851c8eeb7139f5d43005b52e5
Lili Boxer <ninaliboxer@gmail.com>
2019-08-15 17:00:16 -0700

    refactored function for target value shenanigans

--- src/components/TextFormatter.js
+++ src/components/TextFormatter.js
@@ -5,37 +5,42 @@ export default class TextFormatter extends Component {
   state = {
     text: '',
     formattedText: '',
-    font: 'Ghost',
+    font: 'Banner',
     color: ''
   }
 
-  handleSubmit = event => {
-    event.preventDefault();
-  }
-
-  handleTextChange = ({ target }) => {
-    let formattedText = '';
-    figlet.text(target.value, {
+  parseText= () => {
+    figlet.text(this.state.text, {
       font: this.state.font
-    }, function(err, data) {
-      formattedText = data;
+    }, (err, data) => {
+      this.setState({ formattedText: data });
     });
-    this.setState({ [target.name]: target.value, formattedText });
-    console.log(formattedText);
   }
 
   handleColorChange = ({ target }) => {
     this.setState({ color: target.value });
   }
-
+  
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value }, () => {
+      this.parseText();
+    });
+  }
+  
   render() {
-    const { text, formattedText, color } = this.state;
+    const { text, formattedText, color, font } = this.state;
+    console.log(formattedText);
+    const fontArr = ['Ghost', 'Arrows', 'Banner', 'Acrobatic'];
+    const mappedFonts = fontArr.map(font => {
+      return <option key={font} value={font}>{font}</option>;
+    });
 
     return (
       <>
-        <form onSubmit={this.handleSubmit}>
+        <form>
           <input type="color" name="color" value={color} onChange={this.handleColorChange}></input>
-          <input placeholder="Type Here" name="text" value={text} onChange={this.handleTextChange}></input>
+          <input placeholder="Type Here" name="text" value={text} onChange={this.handleChange}></input>
+          <select name="font" defaultValue={font} onChange={this.handleChange}>{mappedFonts}</select>
         </form>
         <pre style={{ color: color } }>{formattedText}</pre>
       </>
